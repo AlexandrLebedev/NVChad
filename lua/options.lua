d9d46b7d56edbd65c0038840c04a7d5b7610c8af
AlexandrLebedev <lebdev.98@inbox.ru>
2025-07-18 15:48:02 +0500

    Delete unused opitons

--- lua/options.lua
+++ lua/options.lua
@@ -1,17 +1,4 @@
 require "nvchad.options"
-local cmp = require "cmp"
-
-cmp.setup {
-  sources = {
-    { name = "nvim_lsp" },
-  },
-}
-
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-require("lspconfig").clangd.setup {
-  capabilities = capabilities,
-}
 
 -- Code for disable lsp ppup menu for autocomplete
 -- require("lspconfig")["gopls"].setup {
